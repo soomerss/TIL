6fb8eada8cc50e813d94a8c22bebf67fad44aa20
soomerss <poqw741@naver.com>
2023-06-03 20:19:19 +0900

    학습내용 추가

--- 알고리즘/python_file/1756.py
+++ 알고리즘/python_file/1756.py
@@ -1,19 +1,26 @@
-import collections
+from collections import deque
 
-n, m = map(int, input().split())
-
+d, n = map(int, input().split())
 oven = list(map(int, input().split()))
-int_d = collections.deque(map(int, input().split()))
+case = list(map(int, input().split()))
 
-index = 0
+case = deque(case)
 
-for i in range(n - 1):
+answer = d
+
+for i in range(len(oven) - 1):
     if oven[i] < oven[i + 1]:
         oven[i + 1] = oven[i]
 
-for i in range(n-1,-1,-1):
-    if int_d[index] <= oven[i]:
+for i in range(d - 1, -1, -1):
+    if oven[i] >= case[0]:
+        case.popleft()
+        answer = i + 1
 
+    if not case:
+        break
 
-
-binary_search(int_d, oven, 0, n - 1)
+if case:
+    print(0)
+else:
+    print(answer)
